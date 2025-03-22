1af2d32c75e40bd344c2bc49f359acce9c3ca0ee
KamleshPatel98 <kamleshp52170@gmail.com>
2025-03-23 00:22:39 +0530

    simple and user defined biinding

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+// use App\Models\Brand;
+
+class UserController extends Controller
+{
+    public function show4(User $user){
+        return $user;
+    }
+
+    public function show5(User $user){
+        return $user;
+    }
+
+    // public function show6(User $user, Brand $brand){
+    //     return $user;
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use \App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::view('component','component');
+
+//Implict Route Model Binding
+//1 simple 
+Route::get('users1/{user}', function (User $user){
+    return $user;
+});
+//2 user defined column
+Route::get('users2/{user:name}', function (User $user){
+    return $user;
+});
+//2 use multiple model
+// Route::get('users3/{user:name}/brands/{brand}', function (User $user, Brand $brand){
+//     return $user->name . $brand->name;
+// });
+// 3 controller in id
+Route::get('users4/{user}',[UserController::class,'show4']);
+// 4 controller in different column
+Route::get('users5/{user:email}',[UserController::class,'show5']);
+// Route::get('users6/{user}',[UserController::class,'show6']);
